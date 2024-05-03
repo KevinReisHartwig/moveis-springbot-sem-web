afb36020e955e4098e020cbeaa0bbb82a5391fc8
Kevin dos Reis Hartwig <kevindosreishartwig1221@gmail.com>
2024-05-03 13:15:41 -0300

    modelando os dados da aplicação

--- src/main/java/br/com/alura/kevin/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/kevin/screenmatch/ScreenmatchApplication.java
@@ -1,8 +1,6 @@
 package br.com.alura.kevin.screenmatch;
 
-import br.com.alura.kevin.screenmatch.model.DadosSerie;
-import br.com.alura.kevin.screenmatch.services.ConsumoApi;
-import br.com.alura.kevin.screenmatch.services.ConverteDados;
+import br.com.alura.kevin.screenmatch.main.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,12 +13,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	}
 
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 
 }
