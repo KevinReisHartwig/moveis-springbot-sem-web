f56f38bb20a92cbb00bc9f522a58a37d3146264c
Kevin dos Reis Hartwig <kevindosreishartwig1221@gmail.com>
2024-07-04 21:47:57 -0300

    persistência de dados e consultas com spring data jpa

--- src/main/java/br/com/alura/kevin/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/kevin/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.alura.kevin.screenmatch;
 
 import br.com.alura.kevin.screenmatch.main.Principal;
+import br.com.alura.kevin.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,12 +10,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
 
+	@Autowired
+	private SerieRepository repositorio;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
 	public void run(String... args) throws Exception {
-		Principal principal = new Principal();
+		Principal principal = new Principal(repositorio);
 		principal.exibeMenu();
 	}
 
